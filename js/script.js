29df8099233842242c15ff267905c6f12f4959de
Aleksandra <haruka.lex@gmail.com>
2022-11-12 00:20:04 +0100

    try to get article id  from variable

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 const titleClickHandler = function(event){
-  
-    /* remove class 'active' from all article links  */
+    event.preventDefault();
+    
+    const clickedElement = this;
+
+    /* [done] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-    
-    /* remove class 'active' from all articles */
+
+    /* [done] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+
+    /* [done] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
+
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    
+    console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    
+    const targetArticle = document.querySelector(".active");
+    console.log(document.querySelector('.active').href);
     /* add class 'active' to the correct article */
+    // targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
